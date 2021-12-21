bcfcbea588f21dbfefbc3124a4fa6046d75f067d
Luca Pisciotta <luca.pisciotta@musement.com>
2021-12-21 17:36:42 +0100

    Fist commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# aws-mfa-profiles
+A package to create credential for AWS using MFA and with the support of multiple profiles
---
+++ aws-mfa-profiles/__main__.py
@@ -0,0 +1,75 @@
+from boto3 import session as boto3_session
+from botocore.session import Session as botocore_session
+from os import environ as os_environ
+import argparse
+import configparser
+import logging
+
+logging.basicConfig(format='{"timestamp":"%(asctime)s", "level":"%(levelname)s", "message":"%(message)s"}')
+parser = argparse.ArgumentParser(description='Set credential to connect on AWS using MFA')
+parser.add_argument('-p', metavar='profile', default='default', help='Profile from which get mfa configuration')
+parser.add_argument('-t', metavar='seconds', type=int, default=43200,
+                    help='Token expiration time in second from 900 (15 minutes) to 129600 (36 hours)')
+
+
+def check_token_validation(prompt: str):
+    if prompt.isdecimal() and 6 <= len(prompt):
+        logging.info('Token has at least 6 characters, this is ok')
+        logging.info('Token is only numerical, this is ok')
+        return
+    else:
+        logging.error('Token is invalid!')
+        quit(code=1)
+
+
+def generate_credentials(aws_account_name: str, aws_token_validity: int, token: str):
+    mfa_session = botocore_session(profile=aws_account_name)
+    mfa_serial = mfa_session.get_scoped_config().get('mfa_serial')
+    client = generate_client_session(aws_account_name)
+    try:
+        response = client.get_session_token(
+            DurationSeconds=aws_token_validity,
+            SerialNumber=mfa_serial,
+            TokenCode=token
+        )
+        return response
+    except Exception as e:
+        logging.error(e)
+        quit(code=1)
+
+
+def generate_client_session(aws_account_name: str):
+    session = boto3_session.Session(
+        profile_name=aws_account_name
+    )
+    client = session.client('sts')
+    return client
+
+
+def set_aws_variables(credentials):
+    config = configparser.ConfigParser()
+    config.read('{}/.aws/credentials'.format(os_environ['HOME']))
+    try:
+        config.remove_section('default')
+    except Exception as e:
+        logging.info('No section to delete ', e)
+    config.add_section('default')
+    config['default']['aws_access_key_id'] = credentials['Credentials']['AccessKeyId']
+    config['default']['aws_secret_access_key'] = credentials['Credentials']['SecretAccessKey']
+    config['default']['aws_session_token'] = credentials['Credentials']['SessionToken']
+    with open('{}/.aws/credentials'.format(os_environ['HOME']), 'w') as configfile:
+        config.write(configfile)
+
+
+def handler():
+    args = parser.parse_args()
+    aws_account_name = args.p
+    aws_token_validity = args.t
+    token = input("Insert MFA token: ")
+    check_token_validation(token)
+    response = generate_credentials(aws_account_name, aws_token_validity, token)
+    set_aws_variables(response)
+
+
+if __name__ == '__main__':
+    handler()
